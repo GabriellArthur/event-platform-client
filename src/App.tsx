53afc0772f72c4f46e81632fb9b51354286725b7
Gabriel Fiusa <gabriel.arthur2@gmail.com>
2022-10-10 09:57:51 -0300

    aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
-import { gql } from "@apollo/client/core"
-import { useQuery } from "@apollo/client/react"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event"
 
 function App() {
-  const { loading, error, data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
-
-  return (
-    <div className="text-2xl font-bold">
-      <ul>
-        {data?.lessons.map(lesson => {return <li key={lesson.id}>{lesson.title}</li>})}
-      </ul>
-    </div>
-  )
+  return <Event/>
 }
 
 export default App
